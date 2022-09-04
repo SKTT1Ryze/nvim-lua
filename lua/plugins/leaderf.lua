47b9774dfb4e4572a372adc5c20548fa28f18aef
SKTT1Ryze <linuxgnulover@gmail.com>
2022-09-04 12:37:24 +0800

    update leaderf

--- lua/plugins/leaderf.lua
+++ lua/plugins/leaderf.lua
@@ -1,6 +1,7 @@
 local M = {}
 local map = vim.api.nvim_set_keymap
 
+
 function M:config()
   vim.g['Lf_WindowPosition'] = 'popup'
   vim.g['Lf_PreviewInPopup'] = 1
@@ -14,7 +15,16 @@ function M:config()
 
   map('n', '<leader>f', ':Leaderf file<CR>', {})
   map('n', '<leader>b', ':Leaderf! buffer<CR>', {})
-  map('n', '<leader>F', ':Leaderf rg', {})
+  map('n', '<leader>F', ':Leaderf rg ', {})
+  map('v', '<D-f>', 'y/<C-r>"<CR>', { noremap = true })
+  map('v', '<leader>f', '<Plug>LeaderfRgVisualLiteralNoBoundary<CR>', {
+    noremap = true,
+    unique = true
+  })
+  map('v', '<leader>F', '<Plug>LeaderfRgVisualLiteralNoBoundary', {
+    noremap = true,
+    unique = true
+  })
 end
 
 return M
