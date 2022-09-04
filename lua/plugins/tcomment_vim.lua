a3af6e0215027411a246ed4df018d34c5e45516a
SKTT1Ryze <linuxgnulover@gmail.com>
2022-09-04 10:58:23 +0800

    update leaderf.lua and tcomment_vim.lua

--- lua/plugins/tcomment_vim.lua
+++ lua/plugins/tcomment_vim.lua
@@ -3,6 +3,7 @@ local map = vim.api.nvim_set_keymap
 
 function M:config()
   vim.g['tcomment_textobject_inlinecomment'] = ''
+
   map('n', '<leader>cn', 'g>c', {})
   map('v', '<leader>cn', 'g', {})
   map('n', '<leader>cu', 'g<c', {})
