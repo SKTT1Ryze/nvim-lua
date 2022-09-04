d257249ba2aea0ea2ab872ba5abf77ace06abb7c
SKTT1Ryze <linuxgnulover@gmail.com>
2022-09-04 12:55:43 +0800

    update fugitter

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -3,6 +3,8 @@ local map = vim.api.nvim_set_keymap
 
 function M:config()
   map('n', '<leader>gb', ':Git blame<CR>', { noremap = true })
+  map('n', '<leader>gl', ':GcLog<CR>', { noremap = true })
+  map('n', '<leader>gd', ':Gdiffsplit<CR>', { noremap = true })
 end
 
 return M
