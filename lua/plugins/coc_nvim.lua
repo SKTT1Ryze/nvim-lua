602cad14f1f9fc37ed9012117b9183079d3bbe7c
SKTT1Ryze <linuxgnulover@gmail.com>
2022-09-04 11:17:35 +0800

    use vim-notify in coc.nvim

--- lua/plugins/coc_nvim.lua
+++ lua/plugins/coc_nvim.lua
@@ -10,6 +10,97 @@ function _G.CheckBackSpace()
     end
 end
 
+local coc_status_record = {}
+
+function _G.coc_status_notify(msg, level)
+  local notify_opts = {
+    title = "LSP Status",
+    timeout = 500,
+    hide_from_history = true,
+    on_close = reset_coc_status_record
+  }
+
+  -- if coc_status_record is not {} then add it to notify_opts to key called "replace"
+  if coc_status_record ~= {} then
+    notify_opts["replace"] = coc_status_record.id
+  end
+  coc_status_record = vim.notify(msg, level, notify_opts)
+end
+
+function _G.reset_coc_status_record(window)
+  coc_status_record = {}
+end
+
+local coc_diag_record = {}
+
+function _G.coc_diag_notify(msg, level)
+  local notify_opts = {
+    title = "LSP Diagnostics",
+    timeout = 500,
+    on_close = reset_coc_diag_record
+  }
+
+  -- if coc_diag_record is not {} then add it to notify_opts to key called "replace"
+  if coc_diag_record ~= {} then
+    notify_opts["replace"] = coc_diag_record.id
+  end
+  coc_diag_record = vim.notify(msg, level, notify_opts)
+end
+
+function _G.reset_coc_diag_record(window)
+  coc_diag_record = {}
+end
+
+function notify_config()
+  vim.cmd([[
+  function! s:DiagnosticNotify() abort
+    let l:info = get(b:, 'coc_diagnostic_info', {})
+    if empty(l:info) | return '' | endif
+    let l:msgs = []
+    let l:level = 'info'
+     if get(l:info, 'warning', 0)
+      let l:level = 'warn'
+    endif
+    if get(l:info, 'error', 0)
+      let l:level = 'error'
+    endif
+   
+    if get(l:info, 'error', 0)
+      call add(l:msgs, ' Errors: ' . l:info['error'])
+    endif
+    if get(l:info, 'warning', 0)
+      call add(l:msgs, ' Warnings: ' . l:info['warning'])
+    endif
+    if get(l:info, 'information', 0)
+      call add(l:msgs, ' Infos: ' . l:info['information'])
+    endif
+    if get(l:info, 'hint', 0)
+      call add(l:msgs, ' Hints: ' . l:info['hint'])
+    endif
+    let l:msg = join(l:msgs, "\n")
+    if empty(l:msg) | let l:msg = ' All OK' | endif
+    call v:lua.coc_diag_notify(l:msg, l:level)
+  endfunction
+  
+  function! s:StatusNotify() abort
+    let l:status = get(g:, 'coc_status', '')
+    let l:level = 'info'
+    if empty(l:status) | return '' | endif
+    call v:lua.coc_status_notify(l:status, l:level)
+  endfunction
+  
+  function! s:InitCoc() abort
+    execute "lua vim.notify('Initialized coc.nvim for LSP support', 'info', { title = 'LSP Status' })"
+  endfunction
+  
+  " notifications
+  autocmd User CocNvimInit call s:InitCoc()
+  autocmd User CocDiagnosticChange call s:DiagnosticNotify()
+  autocmd User CocStatusChange call s:StatusNotify()
+
+  ]])
+end
+
 function M:config()
   vim.g.coc_global_extenstions = {
     'coc-highlight',
@@ -21,6 +112,7 @@ function M:config()
   }
 
   vim.opt.signcolumn = 'number'
+  vim.opt.updatetime = 300
 
   -- <TAB> to select candidate forward or
   -- pump completion candidate
@@ -55,6 +147,8 @@ function M:config()
 
   -- Go Back
   map('n', 'gb', '<C-o>', {silent = true})
+
+  notify_config()
 end
 
 return M
