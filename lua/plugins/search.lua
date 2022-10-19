88004e1fe590ceaa5aa5ecf30a4d009c2ad2db94
chechunchi <chechunchi.ccc@bytedance.com>
2022-10-19 10:59:31 +0800

    add telescope

--- lua/plugins/search.lua
+++ lua/plugins/search.lua
@@ -1,7 +1,6 @@
 local M = {}
 local map = vim.api.nvim_set_keymap
 
-
 function M:config()
   vim.g['Lf_WindowPosition'] = 'popup'
   vim.g['Lf_PreviewInPopup'] = 1
@@ -13,9 +12,15 @@ function M:config()
     ['<C-j>'] = {'<C-n>'}
   }
 
-  map('n', '<leader>f', ':FZF<CR>', {})
-  map('n', '<leader>b', ':Buffers<CR>', {})
+  -- map('n', '<leader>f', ':FZF<CR>', {})
+  -- map('n', '<leader>b', ':Buffers<CR>', {})
+  
+  local builtin = require('telescope.builtin')
+  vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+  vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+  
   map('n', '<leader>F', ':Leaderf rg ', {})
+  
   map('v', '<D-f>', 'y/<C-r>"<CR>', { noremap = true })
   map('v', '<leader>f', '<Plug>LeaderfRgVisualLiteralNoBoundary<CR>', {
     noremap = true,
