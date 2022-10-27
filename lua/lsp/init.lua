3d5ef8bbfbbbbfb37b207a1cca2b48cd7417c189
chechunchi <chechunchi.ccc@bytedance.com>
2022-10-27 11:21:21 +0800

    add clangd

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -12,7 +12,7 @@ function Lsp:config()
   })
 
   require("mason-lspconfig").setup({
-    ensure_installed = { "sumneko_lua", "rust_analyzer", "tsserver", "gopls", "vimls" }
+    ensure_installed = { "sumneko_lua", "rust_analyzer", "tsserver", "gopls", "vimls", "clangd" }
   })
 
   -- Mappings.
